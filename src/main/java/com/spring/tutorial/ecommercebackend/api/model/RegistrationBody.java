4dac162e28fcf173474084ffcd06711f03520a6a
ShirelleW <shirellewinecoffm@gmail.com>
2024-02-01 17:01:38 -0500

    Added validation for user/pass/email/etc in RegBody model

--- src/main/java/com/spring/tutorial/ecommercebackend/api/model/RegistrationBody.java
+++ src/main/java/com/spring/tutorial/ecommercebackend/api/model/RegistrationBody.java
@@ -1,15 +1,30 @@
 package com.spring.tutorial.ecommercebackend.api.model;
 
-import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.*;
 
 public class RegistrationBody {
 
+    @NotNull
+    @NotBlank
+    @Size(min=3, max=255)
     private String username;
-//  part of Validation dep.
+    //  part of Validation dep.
+    @NotNull
+    @NotBlank
     @Email
     private String email;
+    @NotNull
+    @NotBlank
+    @Size(min=8, max=255)
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")
     private String password;
+    @NotNull
+    @NotBlank
+    @Size(min=2, max=255)
     private String firstName;
+    @NotNull
+    @NotBlank
+    @Size(min=2, max=255)
     private String lastName;
 
     public String getUsername() {
